24baa7fda69ce02dc960c5ec59290e4d8323ecf6
ajcairel <alexanderjcairel@gmail.com>
2022-10-03 14:16:58 -0700

    Need sto fix Snapchat stacking on landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
+import './LandingPage.css';
+
+
 export default function LandingPage() {
     return (
+        <>
+        <div id="heading-text">
+            <h1 id="header">
+                Imagine if 
+                <span id="snapchat">Snapchat</span> had events.
+            </h1>
+            <h3 id="description">
+            Easily host and share events with your friends across any social media.
 
-        <div className="bg-envBG">
-            <h1>Hello World</h1>
-
+            </h3>
+        
         </div>
+        <div id="landing-image">
+            <img src="./LandingImage.png" alt="invite" />
+        </div>
+
+        <Link to="/create">
+            <button id="create-button">Create My Event</button>
+        </Link>
+
+        </>
     );
 }
